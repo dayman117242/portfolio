5020ec613786a53290827ed864dde2027ce8287c
dayman117242 <dayman117242@users.noreply.github.com>
2022-10-19 18:43:03 -0400

    Update

---
+++ sketch/sketch.js
@@ -0,0 +1,32 @@
+var x;
+var y;
+
+function setup() {
+    createCanvas(800, 400);
+    x = width / 2
+    y = height / 2;
+    rectMode(CENTER);
+}
+
+function draw() {
+    if (mouseX > x) {
+        x += 3;
+    }
+    if (mouseX < x) {
+        x -= 3;
+    }
+    if (mouseY > y) {
+        y += 3;
+    }
+    if (mouseY < y) {
+        y -= 3;
+    }
+    
+    background(0)
+    stroke(3, 252, 177);
+    strokeWeight(10);
+    line(x, 0, x, height);
+    line(0, y, width, y);
+    noFill();
+    rect(mouseX, mouseY, 100, 100);
+}
