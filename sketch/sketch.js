8c8f84f72faeec91e351b88fbebf7b547af1eb5c
dayman117242 <dayman117242@users.noreply.github.com>
2022-10-19 20:38:58 -0400

    Update Sketch

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -2,7 +2,7 @@ var x;
 var y;
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(800, 400);
     x = width / 2
     y = height / 2;
     rectMode(CENTER);
@@ -21,6 +21,8 @@ function draw() {
     if (mouseY < y) {
         y -= 3;
     }
+  
+    let d = dist(mouseX, mouseY, x, y);
     
     background(0)
     stroke(3, 252, 177);
@@ -28,5 +30,5 @@ function draw() {
     line(x, 0, x, height);
     line(0, y, width, y);
     noFill();
-    rect(mouseX, mouseY, 100, 100);
+    rect(mouseX, mouseY, d + 25, d + 25);
 }
